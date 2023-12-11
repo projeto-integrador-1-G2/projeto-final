53331fe46be1b2a2012e0d973d83d6ee80a65bb1
Bruno Ribeiro <bbrunoo@icloud.com>
2023-12-11 16:37:12 -0300

    shell

---
+++ codigo/runner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 simulacao.py < simulacao
+python3 simulacao.py < simulacao
+python3 simulacao.py < simulacao
+python3 lancamento.py < time
+python3 update_markdown.py
---
+++ commiter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git pull origin master
+git add /docs/lancamentos.md
+git commit -m "Updates on pages."
+git push origin master
